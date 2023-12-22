8a8331c400e5ddec3046502cae3f91c3b84f198e
SevLG <sebastian.gunnari.lindbom@scilifelab.se>
2023-12-22 17:56:02 +0100

    Adding another lib for rendering markdown links, footnotes and such.

--- react-app/src/components/ArticleComponent.tsx
+++ react-app/src/components/ArticleComponent.tsx
@@ -1,2 +1,12 @@
-export default function ArticleComponent() {}
-  
+import Markdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+
+export default function ArticleComponent() {
+    const markdown = `Just a link: www.nasa.gov.`;
+
+    return (
+        <Markdown remarkPlugins={[remarkGfm]}>{markdown}</Markdown>
+    );
+}
+
+// continue from here https://stackoverflow.com/questions/42928530/how-do-i-load-a-markdown-file-into-a-react-component
