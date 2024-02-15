f34024286fcb78ec62b90f58e8adcab628b4d3da
SevLG <sebastian.gunnari.lindbom@scilifelab.se>
2024-02-15 11:56:28 +0100

    Initial setup of cookies package and setting basic cookie in App.tsx

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,10 +1,18 @@
 //import React from 'react';
-import { ReactElement } from 'react';
+import { ReactElement, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import FooterComponent from './components/FooterComponent';
 import HeaderComponent from './components/HeaderComponent';
+import Cookies from 'js-cookie';
 
 export default function App(): ReactElement {
+
+  const [isTrackingCookieSet, setTrackingCookie] = useState(!(Cookies.get('trackingEnabled') === undefined));
+  if (!isTrackingCookieSet) {
+    Cookies.set('trackingEnabled', 'true');
+    setTrackingCookie(true);
+  };
+
   return (
     <div data-theme="dark">
       <HeaderComponent />
