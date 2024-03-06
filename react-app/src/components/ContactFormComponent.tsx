4d8c59b261bf008845cfae3c9fba05d04581dd92
SevLG <sebastian.gunnari.lindbom@scilifelab.se>
2024-03-06 17:35:26 +0100

    Got the basic functionality in, it now tells you if your input is wrong while you write and the onSubmit function correctly notices if there are errors.

--- react-app/src/components/ContactFormComponent.tsx
+++ react-app/src/components/ContactFormComponent.tsx
@@ -1,85 +1,97 @@
 import { ChangeEvent, FormEvent, ReactElement, useState } from "react";
 import { BUTTON_TYPE_ONE } from "../constants";
+import React from "react";
 
 export default function ContactFormComponent(): ReactElement {
     const [inputFields, setInputFields] = useState({
-        firstName: "",
-        lastName: "",
+        name: "",
         email: "",
         message: ""
     });
     const [errors, setErrors] = useState({
-        firstName: "",
-        lastName: "",
+        name: "",
         email: "",
         message: ""
     });
 
     const validateValues = (inputValues: {[key: string]: string}) => {
-        setErrors({
-            firstName: "",
-            lastName: "",
+        let errors_tmp = {
+            name: "",
             email: "",
             message: ""
-        });
-
-        if (!inputValues.firstName.match(/^[A-Za-zŽžÀ-ÿ]+$/)) {
-            setErrors({ ...errors, ["firstName"]: "Invalid character in name."});
-        }
-
-        if (!inputValues.lastName.match(/^[A-Za-zŽžÀ-ÿ]+$/)) {
-            setErrors({ ...errors, ["lastName"]: "Invalid character in name."});
+        };
+        if (!inputValues.name.match(/^[A-Za-zŽžÀ-ÿ]+$/)) {
+            errors_tmp.name = "Invalid character in name.";
         }
 
         if (!inputValues.email.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
-            setErrors({ ...errors, ["email"]: "Invalid E-Mail adress."});
+            errors_tmp.email = "Invalid E-Mail adress.";
         }
 
         if (inputValues.message.length > 10) {
-            setErrors({ ...errors, ["message"]: "Message exceeds limit."});
+            errors_tmp.message = "Message exceeds limit.";
         }
+        let key: keyof typeof errors;
+        for (key in errors) {
+            console.log(key);
+            setErrors({ ...errors, [key]: errors_tmp[key]})
+        };
+
     };
 
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        validateValues(inputFields);
+        let validForm = true;
+        let key: keyof typeof errors;
+        for (key in errors) {
+            if (errors[key]) {
+                //console.log(errors[key]);
+                validForm = false;
+            };
+        };
+        console.log(errors);
+        validForm ? console.log(inputFields) : console.log("WRONG!!!")
     };
     
     function handleChange(e: ChangeEvent<HTMLInputElement>) { 
         setInputFields({ ...inputFields, [e.target.name]: e.target.value});
     }
 
+    React.useEffect(() =>{
+        let errors_tmp = {
+            name: "",
+            email: "",
+            message: ""
+        };
+        if (!inputFields.name.match(/^[A-Za-zŽžÀ-ÿ]+$/) && inputFields.name.length > 0) {
+            errors_tmp.name = "Invalid character in name.";
+        }
+
+        if (!inputFields.email.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/) && inputFields.email.length > 0) {
+            errors_tmp.email = "Invalid E-Mail adress.";
+        }
+
+        if (inputFields.message.length > 10 && inputFields.message.length > 0) {
+            errors_tmp.message = "Message exceeds limit.";
+        }
+        setErrors({ name: errors_tmp.name, email: errors_tmp.email, message: errors_tmp.message });
+    }, [inputFields]);
 
     return(
         <form onSubmit={handleSubmit} className="bg-white">
             <label>First Name</label>
             <input 
                 type="text"
-                name="firstName"
+                name="name"
                 placeholder="Type here" 
                 className="input bg-white input-bordered w-full max-w-xs"
-                defaultValue={inputFields.firstName} 
+                defaultValue={inputFields.name} 
                 onChange={handleChange} 
                 required 
             />
-            {errors.firstName ? (
+            {errors.name ? (
                 <p className="error">
-                    {errors.firstName}
-                </p>
-                ) : null}
-            <label>Last Name</label>
-            <input 
-                type="text"
-                name="lastName"
-                placeholder="Type here" 
-                className="input bg-white input-bordered w-full max-w-xs"
-                defaultValue={inputFields.lastName} 
-                onChange={handleChange} 
-                required 
-            />
-            {errors.lastName ? (
-                <p className="error">
-                    {errors.lastName}
+                    {errors.name}
                 </p>
                 ) : null}
             <label>E-Mail</label>
