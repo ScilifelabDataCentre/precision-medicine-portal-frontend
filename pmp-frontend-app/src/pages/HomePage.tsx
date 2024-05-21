260b885373e19bf56cc8504bfeda0ad05d2dc191
SevLG <sebastian.gunnari.lindbom@scilifelab.se>
2024-05-21 10:54:25 +0200

    Commented out unused code, image won't be built when there are minor warnings

--- pmp-frontend-app/src/pages/HomePage.tsx
+++ pmp-frontend-app/src/pages/HomePage.tsx
@@ -1,56 +1,59 @@
 import { ReactElement } from 'react';
-import CardComponent from "../components/CardComponent";
+// import CardComponent from "../components/CardComponent";
 import ImageCarouselAlternativeComponent from "../components/ImageCarouselAlternativeComponent";
-import { BODY_CLASSES, BUTTON_TYPE_ONE, H_1} from '../constants';
-import { ICardConfig, ICardContent } from '../interfaces/types';
+import { BODY_CLASSES, 
+        // BUTTON_TYPE_ONE, 
+        // H_1
+    } from '../constants';
+// import { ICardConfig, ICardContent } from '../interfaces/types';
 import { TrackPageViewIfEnabled } from '../util/cookiesHandling';
 
 export default function HomePage(): ReactElement {
     TrackPageViewIfEnabled();
 
-    var cardTitleClasses: string = "text-center text-base-100-content text-xl font-semibold";
-    var cardTextClasses: string = "text-center";
+    // var cardTitleClasses: string = "text-center text-base-100-content text-xl font-semibold";
+    // var cardTextClasses: string = "text-center";
 
-    var cardConfig: { [id: string] : ICardConfig; } = {
-        'whiteCard': { 
-            cardClasses: "w-[40rem] bg-base-100 text-base-100-content bg-opacity-95 shadow-xl border-2 border-base-100", 
-            titleClasses: cardTitleClasses, 
-            subTitleClasses: "",
-            textClasses: cardTextClasses,
-            imgClasses: "", 
-            buttonClasses: "", 
-            buttonPlacement: "",  
-        },
-        'blackCard': { 
-            cardClasses: "bg-neutral bg-opacity-95 rounded-[10px] shadow border-2", 
-            titleClasses: cardTitleClasses, 
-            subTitleClasses: "",
-            textClasses: cardTextClasses,
-            imgClasses: "",
-            buttonClasses: BUTTON_TYPE_ONE, 
-            buttonPlacement: "justify-center", 
-        }
-    };
+    // var cardConfig: { [id: string] : ICardConfig; } = {
+    //     'whiteCard': { 
+    //         cardClasses: "w-[40rem] bg-base-100 text-base-100-content bg-opacity-95 shadow-xl border-2 border-base-100", 
+    //         titleClasses: cardTitleClasses, 
+    //         subTitleClasses: "",
+    //         textClasses: cardTextClasses,
+    //         imgClasses: "", 
+    //         buttonClasses: "", 
+    //         buttonPlacement: "",  
+    //     },
+    //     'blackCard': { 
+    //         cardClasses: "bg-neutral bg-opacity-95 rounded-[10px] shadow border-2", 
+    //         titleClasses: cardTitleClasses, 
+    //         subTitleClasses: "",
+    //         textClasses: cardTextClasses,
+    //         imgClasses: "",
+    //         buttonClasses: BUTTON_TYPE_ONE, 
+    //         buttonPlacement: "justify-center", 
+    //     }
+    // };
 
-    var cardContent: { [id: string] : ICardContent } = {
-        'whiteCard1': {
-            title: "",
-            subTitle: "",
-            text: "In development", 
-            buttonText: "",
-            imageSrc: "",
-            imageAlt: "",
-        },
-        'blackCard1': {
-            title: "Data sources", 
-            subTitle: "",
-            text: "In development", 
-            buttonText: "Sign In",
-            imageSrc: "",
-            imageAlt: "",
-        },
+    // var cardContent: { [id: string] : ICardContent } = {
+    //     'whiteCard1': {
+    //         title: "",
+    //         subTitle: "",
+    //         text: "In development", 
+    //         buttonText: "",
+    //         imageSrc: "",
+    //         imageAlt: "",
+    //     },
+    //     'blackCard1': {
+    //         title: "Data sources", 
+    //         subTitle: "",
+    //         text: "In development", 
+    //         buttonText: "Sign In",
+    //         imageSrc: "",
+    //         imageAlt: "",
+    //     },
 
-    };
+    // };
 
     return (
         <div>
