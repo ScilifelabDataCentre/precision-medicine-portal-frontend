95f74693dc53fdb104d2c53ad4681e4fdff93315
Jan Lorenz <jan.lorenz@scilifelab.se>
2024-10-08 13:11:08 +0200

    Updated cookieshandling with current dev branch
    Signed-off-by: Jan Lorenz <jan.lorenz@scilifelab.se>

--- next-app/src/util/cookiesHandling.ts
+++ next-app/src/util/cookiesHandling.ts
@@ -1,17 +1,12 @@
+"use client";
+
+import { hasCookie } from "cookies-next";
 import { useMatomo } from "@jonkoops/matomo-tracker-react";
-import Cookie from "js-cookie";
 import React from "react";
 
 // cookies are stored as strings, so they cannot be directly used as boolean type
-export function cookieIsSetToTrue(cookieName: string): Boolean {
-  let cookieValue: string | undefined = Cookie.get(cookieName);
-  /* if (!cookieValue) {
-    throw new Error(
-      'Cookie with name "' + cookieName + '" does not exist or is not set yet.'
-    );
-  } else { */
-  return Cookie.get(cookieName) === "true";
-  // }
+export function trackingDisabled(): boolean {
+  return hasCookie("trackingDisabled");
 }
 
 export function TrackPageViewIfEnabled() {
@@ -21,8 +16,8 @@ export function TrackPageViewIfEnabled() {
   // track page visit if trackingEnabled cookie is set to 'true'
 
   React.useEffect(() => {
-    if (cookieIsSetToTrue("trackingEnabled")) {
+    if (!trackingDisabled()) {
       trackPageView();
     }
-  }, []);
+  });
 }
