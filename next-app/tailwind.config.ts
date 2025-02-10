6ab41d459dfbcc7bbca6a8f3c2d53123459c2057
JanProgrammierung <jan.lorenz@scilifelab.se>
2025-02-10 10:52:57 +0100

    Added lato font as default in Tailwind and globals.css. This should hopefully override default font in shadcn components but double check still required and if needed, customization of the components
    Signed-off-by: JanProgrammierung <jan.lorenz@scilifelab.se>

--- next-app/tailwind.config.ts
+++ next-app/tailwind.config.ts
@@ -72,6 +72,9 @@ export default {
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
+      fontFamily: {
+        sans: ["Lato", "sans-serif"],
+      },
     },
   },
   plugins: [
