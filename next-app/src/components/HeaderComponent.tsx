3289b8bf2ddb2168869c7aa319eda6f1689e1793
JanProgrammierung <jan.lorenz@scilifelab.se>
2025-02-26 14:26:34 +0100

    feature: added header component. Responsive parts still need to be fixed
    Signed-off-by: JanProgrammierung <jan.lorenz@scilifelab.se>

--- next-app/src/components/HeaderComponent.tsx
+++ next-app/src/components/HeaderComponent.tsx
@@ -1,14 +1,8 @@
 "use client";
 
-import { ILink } from "@/interfaces/types";
-import { LINK_CLASSES } from "@/constants";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import clsx from "clsx";
-import { ReactElement } from "react";
-
 import * as React from "react";
-
+import Link from "next/link";
+import Image from "next/image";
 import { cn } from "@/lib/utils";
 import {
   NavigationMenu,
@@ -20,240 +14,138 @@ import {
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 
-export default function HeaderComponent(): ReactElement {
-  const components: { title: string; href: string; description: string }[] = [
-    {
-      title: "Alert Dialog",
-      href: "/docs/primitives/alert-dialog",
-      description:
-        "A modal dialog that interrupts the user with important content and expects a response.",
-    },
-    {
-      title: "Hover Card",
-      href: "/docs/primitives/hover-card",
-      description:
-        "For sighted users to preview content available behind a link.",
-    },
-    {
-      title: "Progress",
-      href: "/docs/primitives/progress",
-      description:
-        "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
-    },
-    {
-      title: "Scroll-area",
-      href: "/docs/primitives/scroll-area",
-      description: "Visually or semantically separates content.",
-    },
-    {
-      title: "Tabs",
-      href: "/docs/primitives/tabs",
-      description:
-        "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-    },
-    {
-      title: "Tooltip",
-      href: "/docs/primitives/tooltip",
-      description:
-        "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
-    },
-  ];
+const components: { title: string; href: string; description: string }[] = [
+  {
+    title: "Our projects",
+    href: "/about/ourprojects",
+    description: "Find out more about our initiatives",
+  },
+  {
+    title: "FAQ",
+    href: "/about/faq",
+    description: "Frequently asked questions by our users",
+  },
+  {
+    title: "Team",
+    href: "/about/team",
+    description: "Who's who in the Data Science Node",
+  },
+  {
+    title: "Partners",
+    href: "/about/partners",
+    description: "Proudly present our partners",
+  },
+];
 
-  const ListItem = React.forwardRef<
-    React.ElementRef<"a">,
-    React.ComponentPropsWithoutRef<"a">
-  >(({ className, title, children, ...props }, ref) => {
-    return (
-      <li>
-        <NavigationMenuLink asChild>
-          <a
-            ref={ref}
-            className={cn(
-              "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-              className
-            )}
-            {...props}
-          >
-            <div className="text-sm font-medium leading-none">{title}</div>
-            <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-              {children}
-            </p>
-          </a>
-        </NavigationMenuLink>
-      </li>
-    );
-  });
-  ListItem.displayName = "ListItem";
-
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
   return (
-    <NavigationMenu>
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-              <li className="row-span-3">
-                <NavigationMenuLink asChild>
-                  <a
-                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                    href="/"
-                  >
-                    <div className="mb-2 mt-4 text-lg font-medium">
-                      shadcn/ui
-                    </div>
-                    <p className="text-sm leading-tight text-muted-foreground">
-                      Beautifully designed components that you can copy and
-                      paste into your apps. Accessible. Customizable. Open
-                      Source.
-                    </p>
-                  </a>
-                </NavigationMenuLink>
-              </li>
-              <ListItem href="/docs" title="Introduction">
-                Re-usable components built using Radix UI and Tailwind CSS.
-              </ListItem>
-              <ListItem href="/docs/installation" title="Installation">
-                How to install dependencies and structure your app.
-              </ListItem>
-              <ListItem href="/docs/primitives/typography" title="Typography">
-                Styles for headings, paragraphs, lists...etc
-              </ListItem>
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Components</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-              {components.map((component) => (
-                <ListItem
-                  key={component.title}
-                  title={component.title}
-                  href={component.href}
-                >
-                  {component.description}
-                </ListItem>
-              ))}
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link href="/docs" legacyBehavior passHref>
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              Documentation
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
+});
+ListItem.displayName = "ListItem";
 
-    /*
+export default function HeaderComponent() {
+  return (
     <header className="bg-gradient-to-b from-secondary to-primary">
-      <div className="text-white 2xl:max-w-screen-2xl 2xl:mx-auto">
-        <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between py-4 px-6">
-          <div className="flex justify-between items-center">
-            <Link href="/">
-              <img
-                className="h-auto w-[600px]"
-                src={"/SciLifeLab logo/Precisionmedicineportal_logo_white.png"}
-                alt="SciLifeLab Logo"
-              />
-            </Link>
-            <button
-              className="lg:hidden text-white focus:outline-none"
-              onClick={toggleMenu}
-            >
-              <svg
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                {isMenuOpen ? (
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                ) : (
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                )}
-              </svg>
-            </button>
-          </div>
-          <nav
-            className={clsx(
-              isMenuOpen ? "block" : "hidden",
-              "lg:block mt-4 lg:mt-0"
-            )}
-          >
-            <ul className="flex flex-col lg:flex-row lg:space-x-6 space-y-2 lg:space-y-0 text-lg">
-              <li className="relative">
-                <button
-                  className="flex items-center text-white focus:outline-none"
-                  onClick={toggleDropdown}
-                >
-                  Data sources
-                  <svg
-                    className="ml-1 h-3 w-3"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
+      <div className="flex items-center justify-between gap-4 2xl:max-w-screen-2xl 2xl:mx-auto py-2 px-6">
+        <Link href="/">
+          <Image
+            width={600}
+            height={59.5}
+            src={"/SciLifeLab logo/Precisionmedicineportal_logo_white.png"}
+            alt="SciLifeLab Logo"
+          />
+        </Link>
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>Data sources</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                  <li className="row-span-3">
+                    <NavigationMenuLink asChild>
+                      <a
+                        className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                        href="/accessclinicaldata"
+                      >
+                        <div className="mb-2 mt-4 text-lg font-medium">
+                          Access clinical data
+                        </div>
+                        <p className="text-sm leading-tight text-muted-foreground">
+                          Navigate human data for research, including clinical
+                          records, quality registries, and biobank data
+                        </p>
+                      </a>
+                    </NavigationMenuLink>
+                  </li>
+                  <ListItem href="/registries" title="Quality registries">
+                    Explore various Swedish registries offering individualised
+                    health data
+                  </ListItem>
+                  <ListItem
+                    href="/swedishresearchprojects"
+                    title="Swedish research projects"
                   >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M19 9l-7 7-7-7"
-                    />
-                  </svg>
-                </button>
-                {isDropdownOpen && (
-                  <ul className="absolute left-0 mt-2 w-48 bg-white text-black py-2 rounded-lg shadow-md z-10">
-                    {dropdownLinks.map((link, index) => (
-                      <li key={index}>
-                        <Link
-                          className={clsx(
-                            "block px-4 py-2 text-sm transition-colors duration-200 hover:bg-gray-100 rounded",
-                            index !== 0 && "pl-8"
-                          )}
-                          href={link.link}
-                          onClick={() => setIsDropdownOpen(false)}
-                        >
-                          {link.text}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                )}
-              </li>
-              {Object.keys(links).map((key) => (
-                <li key={key}>
-                  <Link
-                    className={clsx(
-                      links[key].classes,
-                      "block",
-                      pathname === links[key].link && "underline"
-                    )}
-                    href={links[key].link}
-                    onClick={() => setIsMenuOpen(false)}
-                  >
-                    {links[key].text}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </nav>
-        </div>
+                    Discover Swedish research projects and databases
+                  </ListItem>
+                  <ListItem href="/datasources" title="Other data sources">
+                    Access a comprehensive overview of data sources
+                  </ListItem>
+                </ul>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link href="/kiarva" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  KIARVA dashboard
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link href="/contact" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Contact
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>About us</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                  {components.map((component) => (
+                    <ListItem
+                      key={component.title}
+                      title={component.title}
+                      href={component.href}
+                    >
+                      {component.description}
+                    </ListItem>
+                  ))}
+                </ul>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
       </div>
     </header>
-    */
   );
 }
